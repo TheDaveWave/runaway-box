f91a56e7c65d81e1cace2116259ff81947386afd
David Lindberg <david.wesley.lindberg@gmail.com>
2022-08-30 14:16:20 -0500

    made another div to display the current position of the cursor on the document

--- scripts/client.js
+++ scripts/client.js
@@ -3,8 +3,18 @@
 $(document).ready(readyNow);
 
 
+function getMouseCoords() {
+    addEventListener('mousemove', (e) => {
+        let x = e.clientX;
+        let y = e.clientY;
+        // console.log(x,y);
+    });
+}
+
+
 
 
 function readyNow() {
     console.log('jQuery ready');
+    getMouseCoords();
 }
