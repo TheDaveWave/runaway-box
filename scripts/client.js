8214bd5e368f586576f108056367c01f9c5a47c9
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-01 14:44:23 -0500

    what am I doing?

--- scripts/client.js
+++ scripts/client.js
@@ -12,18 +12,18 @@ function getMouseCoords() {
         let x = e.clientX;
         let y = e.clientY;
         // console.log(x,y);
-        $('#mouse-position').html(`( ${x} ), ( ${y} )`);
-        let el = $('#runaway-box');
-        let elCoords = el.offset();
+        $('#mouse-position').html(`( ${x} ), ( ${y} )`); 
+        
+        
+        let el = $('#run-away');
+        let elBounding = el.getBoundingClienRect();
 
-        let top = Math.random(20) * height;
-        let left = Math.random(20) * width;
+        let elBoundLeft = el.outerWidth;
+        let elBoundTop = el.outerHeight;
 
-        el.html(`( ${elCoords.top} , ${elCoords.left} )`);
-        // el.offset({top: el.offset().top,left: el.offset().left});
-        // console.log(el.offset().top, el.offset().left);
+        
 
-        el.off({top: el.offset().top + top, left: el.offset().left - left});
+
     });
 }
 
@@ -31,10 +31,23 @@ function getMouseCoords() {
 
 // figure out the the mouse is within range of element
 // return true if it is.
-function checkProximity () {
+function runAway() {
     let el = $('#runaway-box');
     let elCoords = el.offset();
+    let top = Math.random(20) * height;
+    let left = Math.random(20) * width;
+
+    el.html(`( ${elCoords.top} , ${elCoords.left} )`);
+        // el.offset({top: el.offset().top,left: el.offset().left});
+        // console.log(el.offset().top, el.offset().left);
+
+    el.css({top: el.offset().top + top, left: el.offset().left - left});
+}
+
+function testBS() {
     
+
+
 }
 
 
