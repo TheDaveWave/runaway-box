851238061e40e9ada7d99daf96ac2528df5d4cdf
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-19 17:16:25 -0500

    got box to move but not correctly

--- scripts/client.js
+++ scripts/client.js
@@ -4,10 +4,11 @@ $(readyNow);
 
 function readyNow() {
     console.log('jQuery ready');
-    getMouseCoords();
+    // getMouseCoords();
+    $('#box-bounds').on('mouseenter', getElementBounds);
     // getElementBounds();
-    runAway();
-    checkMouseOver();
+    // runAway();
+    // checkMouseOver();
 }
 
 const width = $(window).width - 100;
@@ -55,91 +56,122 @@ function checkMouseOver () {
 // needs mouse coords and the position of the sides?
 // needs the distance between the boundaries and the mouse^
 // pass in the mouse event??
-function getElementBounds (mouseEvent) {
-    
-    let el = (document).getElementById('box-bounds');
-    let elBounding = el.getBoundingClientRect();
+function getElementBounds () {
 
-    let mouseX = mouseEvent.pageX;
-    let mouseY = mouseEvent.pageY;
-    // distance should just be a few calculations using the above and below
-    // variables.
-    let elTop = elBounding.top;
-    let elBottom = elBounding.bottom;
-    let elRight = elBounding.right;
-    let elLeft = elBounding.left;
+    addEventListener('mousemove', (mouse) => {
+        let el = (document).getElementById('box-bounds');
+        let elBounding = el.getBoundingClientRect();
 
-    // calculate the distance from the sides.
-    let distTop = Math.abs(elTop - mouseY);
-    let distBottom = Math.abs(elBottom - mouseY);
-    let distRight = Math.abs(elRight - mouseX);
-    let distLeft = Math.abs(elLeft - mouseX);
+        let mouseX = mouse.pageX;
+        let mouseY = mouse.pageY;
 
-    // console.log('Distance from top:',distTop);
-    // console.log('Distance from bottom:',distBottom);
-    // console.log('Distance from right:',distRight);
-    // console.log('Distance from left:',distLeft);
+        // distance should just be a few calculations using the above and below
+        // variables.
+        let elTop = elBounding.top;
+        let elBottom = elBounding.bottom;
+        let elRight = elBounding.right;
+        let elLeft = elBounding.left;
 
-    let distMin = Math.min(distTop,distBottom,distRight,distLeft);
-    // console.log('******************');
-    // should give us the value of the closest side.
-    // console.log('Min:',distMin);
+        // calculate the distance from the sides.
+        let distTop = Math.abs(elTop - mouseY);
+        let distBottom = Math.abs(elBottom - mouseY);
+        let distRight = Math.abs(elRight - mouseX);
+        let distLeft = Math.abs(elLeft - mouseX);
 
-    // log what side is the closest.
-    let side = '';
+        // console.log('Distance from top:',distTop);
+        // console.log('Distance from bottom:',distBottom);
+        // console.log('Distance from right:',distRight);
+        // console.log('Distance from left:',distLeft);
 
-    switch (distMin) {
-        case distTop:
-            side = "Top";
-            break;
-        case distBottom:
-            side = "Bottom";
-            break;
-        case distLeft:
-            side = "Left";
-            break;
-        case distRight:
-            side = "Right";
-            break;
-        default:
-            console.log("error");
-    }
+        let distMin = Math.min(distTop,distBottom,distRight,distLeft);
+        // console.log('******************');
+        // should give us the value of the closest side.
+        // console.log('Min:',distMin);
 
-    // for some reason if you are far enough from the bottom or top
-    // it logs the left or right respectively.
-    console.log('***** Closest Side *****');
-    console.log(side);
+        // log what side is the closest.
+        let side = '';
+
+        switch (distMin) {
+            case distTop:
+                side = "Top";
+                break;
+            case distBottom:
+                side = "Bottom";
+                break;
+            case distLeft:
+                side = "Left";
+                break;
+            case distRight:
+                side = "Right";
+                break;
+            default:
+                console.log("error");
+        }
+
+        // for some reason if you are far enough from the bottom or top
+        // it logs the left or right respectively.
+        console.log('***** Closest Side *****');
+        console.log(side);
 
 
-    // console.log('* Element Bounding: Box-Bounds *');
-    // console.log(elBounding);
-    // console.log('Bounding Top:',elTop);
-    // console.log('Bounding Right:',elRight);
-    // console.log('Bounding Bottom:',elBottom);
-    // console.log('Bounding Left:',elLeft);
-    
+        // console.log('* Element Bounding: Box-Bounds *');
+        // console.log(elBounding);
+        // console.log('Bounding Top:',elTop);
+        // console.log('Bounding Right:',elRight);
+        // console.log('Bounding Bottom:',elBottom);
+        // console.log('Bounding Left:',elLeft);
+        
+        moveBox(side, mouse);
     // return side;
+    });
+
 }
 
 // move the box relative to which side the mouse is closest to.
-function moveBox(side) {
+function moveBox(side, mouseEvent) {
+    // get the boundaries of the bounary box.
+    let el = $('#box-bounds');
+    let mouse = mouseEvent;
+
+    // get the width and height of the boundary box
+    // so we can move box knowing how getBoundingClientRect()
+    // diagram.
+    let width = el.outerWidth();
+    let height = el.outerHeight();
+
+    // get Mouse position.
+    // let mouseX = mouseEvent.pageX;
+    // let mouseY = mouseEvent.pageY;
+
 
     switch(side) {
         case 'Top':
             // if mouse enters the border move the box
             // away from top.
+            el.css({
+               top: mouse.pageY + 20
+            });
             break;
         case 'Bottom':
             // if mouse enters the border move box
             // towards the top.
+            el.css({
+                top: mouse.pageY - height - 20
+            });
             break;
         case 'Left':
             // if mouse enters the border move box
             // away from the left.
+            el.css({
+                left: mouse.pageX + 20
+            });
             break;
         case 'Right':
             // if mouse enters the border move box
             // toward the left.
+            el.css({
+                left: mouse.pageX - width - 20
+            });
             break;
         defualt:
             console.log('Error getting side');
