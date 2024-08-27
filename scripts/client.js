a8c86793147ee9feb494b75ff36582e0a4fc6aa9
David Lindberg <david.wesley.lindberg@gmail.com>
2024-08-27 13:59:34 -0500

    got movement down correctly, need to clean up some functions

--- scripts/client.js
+++ scripts/client.js
@@ -17,28 +17,23 @@ function onReady(event) {
 
   // // temporary listener to log the angle of the mouse relative to the box.
   // document.addEventListener("mousemove", function (event) {
-  //   console.log("ANGLE:::", getAngle(event));
-  //   // let b = document.getElementById('box-bounds');
-  //   // b.innerText = getAngle(event) + " deg"
+  //   // console.log("ANGLE:::", getAngle(event));
+  //   let b = document.getElementById('runaway-box');
+  //   b.innerText = getAngle(event) + " deg"
   // });
 }
 
-// // Function to set the app's mode to the mode the user has selected: dark or light.
-// function setDarkMode() {
-//   window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", ({matches}) => {
-//     if(matches) {
-//       document.body.classList.add('dark-mode');
-//       document.getElementById("runaway-box").classList.add('dark-box');
-//     } else {
-//       document.body.classList.remove('dark-mode');
-//       document.getElementById("runaway-box").classList.remove('dark-box');
-//     }
-//   })
-// }
-
 // Function to set the app's mode to the mode the user has selected: dark or light.
 function getDarkMode() {
-  const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  setDarkMode();
+  window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", ({matches}) => {
+    setDarkMode(matches);
+  });
+}
+
+// Function to set the app's mode to the mode the user has selected: dark or light.
+function setDarkMode(matches) {
+  const isDarkMode = matches ?? window.matchMedia("(prefers-color-scheme: dark)").matches;
   if(isDarkMode) {
     document.body.classList.add('dark-mode');
     document.getElementById("runaway-box").classList.add('dark-box');
@@ -53,46 +48,34 @@ function getDarkMode() {
 
 // gets the bounds of the page to keep the box from trailing off the page.
 function getDocumentBorder() {
-  // get the max width using math.max on every possible way to get the width
-  // to improve compatability with different browsers.
-  let docX = Math.max(
-    document.body.offsetWidth,
-    document.documentElement.offsetWidth,
-    document.documentElement.clientWidth
-  );
+  // get the max width using math.max on every possible way to get the width to improve compatability with different browsers.
+  let docX = Math.max(document.body.offsetWidth, document.documentElement.offsetWidth, document.documentElement.clientWidth);
   // get the max height.
-  let docY = Math.max(
-    document.body.offsetHeight,
-    document.documentElement.offsetHeight,
-    document.documentElement.clientHeight
-  );
-
-  // console.log(docX, docY);
-  const boundary = {
-    docuWidth: docX,
-    docuHeight: docY,
-  };
-
-  return boundary;
+  let docY = Math.max(document.body.offsetHeight, document.documentElement.offsetHeight, document.documentElement.clientHeight);
+  return { docuWidth: docX, docuHeight: docY};
 }
 
 function getAngle(event) {
   // get the boundaries of the boundary box.
   const b = document.getElementById('box-bounds').getBoundingClientRect();
-  // because the top and left values change, the angle calculation changes...
-  // Maybe need to get the absolute center of the element.
+  // need to get the absolute center of the element.
   const x = b.left + b.width / 2;
   const y = b.top + b.height / 2;
   const radians = Math.atan2((y - event.pageY), (x - event.pageX));
-  // let radians = Math.atan2((b.top - event.pageX), (b.left - event.pageY));
   let degree = Math.round(radians * (180 / Math.PI));
   if(degree < 0) { degree = (degree + 360) % 360 }
-  // 0 or 360 degrees is the top left of the box.
   return degree;
 }
 
 function getCartesianCoords(degrees, num) {
-  return {x: Math.round(num * Math.cos(degrees)), y: Math.round(num * Math.sin(degrees))}
+  return {x: Math.round(num * Math.cos(degrees)), y: Math.round(num * Math.sin(degrees))};
+}
+
+function getDeltaCoords(degrees) {
+  const oppositeAngle = (degrees + 180) % 360;
+  // const radians = oppositeAngle * (Math.PI / 180);
+  const radians = degrees * (Math.PI / 180);
+  return {deltaX: Math.round(Math.cos(radians) * 20), deltaY: Math.round(Math.sin(radians) * 20)};
 }
 
 // move the box relative to which side the mouse is closest to it.
@@ -104,67 +87,48 @@ function moveBox(event) {
 
   // get the boundaries of the boundary box.
   let el = document.getElementById('box-bounds');
-  let elBoundary = el.getBoundingClientRect();
-
-  // get the width and height of the boundary box
-  // so we can move box knowing how getBoundingClientRect() diagram works.
-  let width = elBoundary.width;
-  let height = elBoundary.height;
-
-  // get the X and Y position of the mouse/cursor on the page.
-  let mouseX = event.pageX;
-  let mouseY = event.pageY;
-
-  // distance should just be a few calculations using the
-  // above and below variables.
-  let elTop = elBoundary.top;
-  let elBottom = elBoundary.bottom;
-  let elRight = elBoundary.right;
-  let elLeft = elBoundary.left;
-
-  // calculate the distance from the sides and store
-  // them in what I will call distance variables.
-  let distTop = Math.abs(elTop - mouseY);
-  let distBottom = Math.abs(elBottom - mouseY);
-  let distRight = Math.abs(elRight - mouseX);
-  let distLeft = Math.abs(elLeft - mouseX);
-
-  // get the minimum distance or one of the four distance variables
-  // that has the shortest distance at any given time.
-  let distMin = Math.min(distTop, distBottom, distRight, distLeft);
 
   const degrees = getAngle(event);
 
   // 45 degrees is top left of box, so 135 is top right
   // need to figure out how to use the angle to move the box in the opposite direction. Back to algebra class...
-  // gotta multiply the movement amount by the math.whatever
   // GET THIS TO RUN ON A FAKE COOKIE PREFERENCES BUTTON.
 
-  if((degrees >= 225 && degrees < 315) || (degrees >= 135 && degrees < 225)) {
-    const {x, y} = getCartesianCoords(degrees, 30);
-    console.log(x, y);
-    Object.assign(el.style, {top:  String(event.pageY - height - Math.abs(y)) +"px", left: String(event.pageX - width - Math.abs(x)) +"px"});
-  } else {
-    const {x, y} = getCartesianCoords(degrees, 15);
-    console.log(x, y);
-    Object.assign(el.style, {top: String(event.pageY + Math.abs(y)) +"px", left: String(event.pageX + Math.abs(x)) +"px"});
-  }
+  const {deltaX, deltaY} = getDeltaCoords(degrees);
+  const x = Number(el.style.left.substring(0, el.style.left.indexOf("px"))) + deltaX;
+  const y = Number(el.style.top.substring(0, el.style.top.indexOf("px"))) + deltaY;
+  console.log(x, y);
 
+  Object.assign(el.style, {top: String(y) +"px", left: String(x) +"px"});
+  console.log(getDeltaCoords(degrees));
+
+  // // LEFT
   // if((degrees >= 315 && degrees < 360) || (degrees >= 0 && degrees < 45)) {
-  //   console.log(x, y)
+  //   const {x, y} = getCartesianCoords(degrees, 15);
+  //   console.log(x, y);
   //   el.style.setProperty("left", String(event.pageX + 15) +"px");
+  //   // Object.assign(el.style, {top: String(event.pageY + Math.abs(y)) +"px", left: String(event.pageX + Math.abs(x)) +"px"});
   // }
-  // if(degrees >= 45 && degrees < 135) {
-  //   console.log(x, y)
-  //   el.style.setProperty("top", String(event.pageY + 15) +"px");
-  // }
+  // // RIGHT
   // if(degrees >= 135 && degrees < 225) {
-  //   console.log(x, y)
+  //   const {x, y} = getCartesianCoords(degrees, 30);
+  //   console.log(x, y);
   //   el.style.setProperty("left", String(event.pageX - width - 30) +"px");
+  //   // Object.assign(el.style, {top: String(event.pageY + Math.abs(y)) +"px", left: String(event.pageX + Math.abs(x)) +"px"});
   // }
+  // // TOP
+  // if(degrees >= 45 && degrees < 135) {
+  //   const {x, y} = getCartesianCoords(degrees, 15);
+  //   console.log(x, y);
+  //   el.style.setProperty("top", String(event.pageY + 15) +"px");
+  //   // Object.assign(el.style, {top:  String(event.pageY - height - Math.abs(y)) +"px", left: String(event.pageX - width - Math.abs(x)) +"px"});
+  // }
+  // // BOTTOM
   // if(degrees >= 225 && degrees < 315) {
-  //   console.log(x, y)
+  //   const {x, y} = getCartesianCoords(degrees, 30);
+  //   console.log(x, y);
   //   el.style.setProperty("top", String(event.pageY - height - 30) +"px");
+  //   // Object.assign(el.style, {top:  String(event.pageY - height - Math.abs(y)) +"px", left: String(event.pageX - width - Math.abs(x)) +"px"});
   // }
 
   // get the document boundary.
@@ -183,7 +147,6 @@ function moveBox(event) {
   const positionCheck = elPos.top <= 0 || elPos.left <= 0 || elPos.top >= boundary.docuHeight - boxHeight || elPos.left >= boundary.docuWidth - boxWidth;
   if (positionCheck) {
     console.log("Hit Boundary", elPos);
-    el.style.setProperty("top", String(boundary.docuHeight / 2) +"px");
-    el.style.setProperty("left", String(boundary.docuWidth / 2) +"px");
+    Object.assign(el.style, {top: String(boundary.docuHeight / 2) +"px", left: String(boundary.docuWidth / 2) +"px"});
   }
 }
